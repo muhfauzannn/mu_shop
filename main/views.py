f4097869127c7284b76b8f676e9748aad4c00eb1
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-09-09 00:32:27 +0700

    feat: implement initial product model and views for main shop page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def show_main(request):
+    # Get all products from database
+    products = Product.objects.all()
+    
+    # Get featured products
+    featured_products = Product.objects.filter(is_featured=True)
+    
+    # Get products by category
+    jerseys = Product.objects.filter(category='jersey')
+    accessories = Product.objects.filter(category='accessories')
+    
+    # Count products
+    total_products = Product.objects.count()
+    featured_count = featured_products.count()
+    
+    context = {
+        'app_name': 'MU Shop',
+        'shop_description': 'Official Manchester United Merchandise Store',
+        
+        # Product data
+        'products': products,
+        'featured_products': featured_products,
+        'jerseys': jerseys,
+        'accessories': accessories,
+        
+        # Statistics
+        'total_products': total_products,
+        'featured_count': featured_count,
+        
+        # Categories for navigation
+        'categories': Product.CATEGORY_CHOICES,
+        
+        # Shop info
+        'currency': 'IDR',
+        'shop_slogan': 'Glory Glory Man United!'
+    }
+
+    return render(request, "main.html", context)
