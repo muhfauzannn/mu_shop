6ca98044525878e286cf6d479bf7edbfbecd367c
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-09-10 00:53:38 +0700

    chore: static

--- mu_shop/settings.py
+++ mu_shop/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,12 +147,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
-# For production
-STATIC_ROOT = BASE_DIR / "staticfiles"
+if PRODUCTION:
+    # Production: hanya gunakan STATIC_ROOT
+    STATIC_ROOT = BASE_DIR / "staticfiles"
+else:
+    # Development: gunakan STATICFILES_DIRS
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
+    STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# WhiteNoise configuration for production
+if PRODUCTION:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
