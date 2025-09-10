92b339c7b5ba28bbb591872edd7d81e3b54cb7e0
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-09-10 11:24:54 +0700

    feat: add ProductAdmin configuration in admin.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category', 'is_featured', 'created_at']
+    list_filter = ['category', 'is_featured', 'created_at']
+    search_fields = ['name', 'description']
+    list_editable = ['is_featured']
+    ordering = ['-created_at']
